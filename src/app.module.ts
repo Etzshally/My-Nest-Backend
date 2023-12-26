d333f3ad6c17ce5fdaba313f20160dc3f5ae6441
Etzshally <shahpaal10@hotmail.com>
2023-12-26 11:06:49 +0500

    `changes`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { databaseProvider } from './dbservice/knex.service';
 import { ProductService } from './services/product.service';
 import {ProductController} from './controllers/product.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController,ProductController],
-  providers: [AppService,ProductService,databaseProvider],
+  controllers: [ProductController],
+  providers: [ProductService,databaseProvider],
   exports:['KnexConnection']
 })
 export class AppModule {}
